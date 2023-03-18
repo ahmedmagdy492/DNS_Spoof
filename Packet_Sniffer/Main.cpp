26a750da92fe3bf59994dd510d4524652288c132
ahmedmagdy492 <41495541+ahmedmagdy492@users.noreply.github.com>
2023-03-18 15:20:36 +0200

    added extracting udp, dns header

--- Packet_Sniffer/Main.cpp
+++ Packet_Sniffer/Main.cpp
@@ -40,28 +40,77 @@ ip_packet;
 #define BUFFER_SIZE_IP (BUFFER_SIZE_PKT - BUFFER_SIZE_ETH)
 #define BUFFER_OFFSET_ETH sizeof(struct pcap_sf_pkthdr)
 #define BUFFER_OFFSET_IP (BUFFER_OFFSET_ETH + BUFFER_SIZE_ETH)
+#define UDP_HEADER_LEN 8
+
+#define LOG 1
+
+#pragma pack(1)
 
 void dump_packets(unsigned char* buffer) {
 	ip_packet* packet = (ip_packet*)buffer;
 
-	printf("IP Version: %u\n", packet->versionAndLength >> 4);
+	int ipLenght = (packet->versionAndLength & 0b00001111)* (packet->versionAndLength >> 4);
 
-	int length = (packet->versionAndLength & 0b00001111)* (packet->versionAndLength >> 4);
+	if (packet->protocol == 6) {
+		// tcp
+	}
+	else if (packet->protocol == 17) {
 
-	printf("Length: %u\n", length);
+		unsigned short sourcePort = *(buffer + ipLenght+1) + *(buffer + ipLenght + 2);
+		unsigned short destPort = *(buffer + ipLenght + 3) + *(buffer + ipLenght + 4);
 
-	printf("Source IP: %u.%u.%u.%u\n", *(buffer + length - 8), *(buffer + length - 7), 
-		*(buffer + length - 6), *(buffer + length - 5));
+		if (destPort == 53) {
+#ifdef LOG
+			printf("IP Version: %u\n", packet->versionAndLength >> 4);
 
-	printf("Dest IP: %u.%u.%u.%u\n", *(buffer + length - 4), *(buffer + length - 3),
-		*(buffer + length - 1), *(buffer + length - 1));
+			printf("Length: %u\n", ipLenght);
+
+			printf("Source IP: %u.%u.%u.%u\n", *(buffer + ipLenght - 8), *(buffer + ipLenght - 7),
+				*(buffer + ipLenght - 6), *(buffer + ipLenght - 5));
+
+			printf("Dest IP: %u.%u.%u.%u\n", *(buffer + ipLenght - 4), *(buffer + ipLenght - 3),
+				*(buffer + ipLenght - 2), *(buffer + ipLenght - 1));
+
+			// udp
+			printf("UDP Hdr:\n");
+
+			// dns
+			printf("DNS:\n");
+
+			printf("source port: %u\n", sourcePort);
+			printf("dest port: %u\n", destPort);
+#endif
+
+			unsigned char udpLength = *(buffer + ipLenght + 4) + *(buffer + ipLenght + 5);
+
+			unsigned short transactionId = *(buffer + ipLenght + UDP_HEADER_LEN+1) + *(buffer + ipLenght + UDP_HEADER_LEN + 2);
+
+#ifdef LOG
+			printf("Transaction ID: %u\n", transactionId);
+#endif
+
+			// TODO: send dns spoof replay
+
+			// domain section
+			unsigned char* dnsRecord = (unsigned char*)(buffer + ipLenght + UDP_HEADER_LEN + 12);
+
+#ifdef LOG
+			for (int i = 0; i < udpLength; i++) {
+				printf("%c", dnsRecord[i]);
+			}
+
+			printf("\n\n\n");
+#endif
+
+		}
+	}
 }
 
-void start_sniff(char* interface_ip) {
+void start_sniffing(char* interface_ip) {
 	WSAData wsaData;
 	int stratupResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (stratupResult != 0) {
-		fprintf(stderr, "while calling WSAStartup: %u", WSAGetLastError());
+		fprintf(stderr, "while calling WSAStartup: %u\n", WSAGetLastError());
 		exit(-1);
 	}
 
@@ -108,7 +157,6 @@ void start_sniff(char* interface_ip) {
 		}
 
 		dump_packets(buffer + BUFFER_OFFSET_IP);
-		printf("\n\n\n");
 	}
 
 	free(buffer);
@@ -125,5 +173,5 @@ int main(int argc, char** argv) {
 		exit(-1);
 	}
 
-	start_sniff(argv[1]);
+	start_sniffing(argv[1]);
 }
